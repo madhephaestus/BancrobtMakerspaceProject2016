0a8e7d3b1fac996f60157c6c0dcea1b2389a7571
Cory <Cory@192.168.9.151>
2016-04-07 11:20:29 -0400

    brokenstubforcad/n/n

--- bodyGenerator.groovy
+++ bodyGenerator.groovy
@@ -2,12 +2,52 @@ import com.neuronrobotics.bowlerstudio.creature.ICadGenerator;
 import com.neuronrobotics.bowlerstudio.creature.CreatureLab;
 import org.apache.commons.io.IOUtils;
 import com.neuronrobotics.bowlerstudio.vitamins.*;
+import com.neuronrobotics.bowlerstudio.physics.*;
+
+	/**
+ * Gets the all dh chains.
+ *
+ * @return the all dh chains
+ */
+public ArrayList<DHParameterKinematics> getLimbDHChains(MobileBase base) {
+	ArrayList<DHParameterKinematics> copy = new ArrayList<DHParameterKinematics>();
+	for(DHParameterKinematics l:base.getLegs()){
+		copy.add(l);	
+	}
+	for(DHParameterKinematics l:base.getAppendages() ){
+		copy.add(l);	
+	}
+	return copy;
+}
 println "Loading STL file"
 // Load an STL file from a git repo
 // Loading a local file also works here
 File servoFile = ScriptingEngine.fileFromGit(
 	"https://github.com/NeuronRobotics/BowlerStudioVitamins.git",
 	"BowlerStudioVitamins/stl/servo/smallservo.stl");
+
+		CSG dyioReference=   (CSG)(ScriptingEngine.inlineGistScriptRun(
+		"fb4cf429372deeb36f52", 
+		"dyioCad.groovy" ,
+		null))
+	
+//	.transformed(new Transform().translateZ(12.0))
+//	.transformed(new Transform().translateX(5.4));
+	
+	//CSG horn=  STL.file(NativeResource.inJarLoad(IVitamin.class,"smallmotorhorn.stl").toPath())
+	CSG horn = new Cube(6,5,12).toCSG();
+	CSG hornBlank = new Cube(6,5,12).toCSG()
+			.toZMax()
+			.toYMin()
+			.movez(4)
+	CSG mountReference = (CSG)(ScriptingEngine.inlineGistScriptRun(
+		"ce4e7c95d516e265b91e",
+		"servoAttachment.groovy" ,
+		[hornBlank]))
+	CSG mountScrewKeepaway = (CSG)(ScriptingEngine.inlineGistScriptRun(
+		"488e0ee249a5c16ae4d8",
+		"moutScrewKeepaway.groovy" ,
+		null))
 // Load the .CSG from the disk and cache it in memory
 CSG servo  = Vitamins.get(servoFile);
 return new ICadGenerator(){
@@ -17,17 +57,70 @@ return new ICadGenerator(){
 
 		return allCad;
 	}
+	
+	private CSG getAppendageMount(){
+
+		
+		return new Cube(10).toCSG();
+	}
 	@Override 
 	public ArrayList<CSG> generateBody(MobileBase b ) {
 		ArrayList<CSG> allCad=new ArrayList<>();
-		double size =40;
-		CSG r2d2 = new Cube(	size,// X dimention
-			size,// Y dimention
-			size//  Z dimention
-			).toCSG()
-		r2d2.setManipulator(b.getRootListener());
-		allCad.add(r2d2);
+		ArrayList<CSG> cutouts=new ArrayList<>();
+		ArrayList<CSG> attach=new ArrayList<>();
+		CSG attachUnion=null;
+		for(DHParameterKinematics l:b.getLegs()){
+			TransformNR position = l.getRobotToFiducialTransform();
+			Transform csgTrans = TransformFactory.nrToCSG(position)
+			cutouts.add(getAppendageMount()
+				.transformed(csgTrans)
+				);
+			CSG attachment = getAttachment()
+				.transformed(csgTrans)
+			attach.add(attachment);
+			if(attachUnion==null){
+				attachUnion=attachment;
+			}else{
+				attachUnion = 	attachUnion.union(attachment)
+			}
+			
+		}
 
+		CSG upperBody = attachUnion.hull()
+		
+		
+						
+		CSG myDyIO=dyioReference
+				.movez(upperBody.getMaxZ()+22.0)
+				.movex(	upperBody.getMaxX()-
+						Math.abs(upperBody.getMinX())+
+						dyioReference.getMaxX()-
+						dyioReference.getMinX())
+				.movey(	upperBody.getMaxY()-
+						Math.abs(upperBody.getMinY())+
+						dyioReference.getMaxY()-
+						Math.abs(dyioReference.getMinY()))
+		upperBody=upperBody
+		.union(myDyIO)
+		.hull()
+		.difference(myDyIO)
+		
+		upperBody= upperBody.difference(cutouts);
+		
+		upperBody= upperBody.union(attach);
+		
+	
+					
+		upperBody.setColor(Color.CYAN);
+		upperBody.setManipulator(base.getRootListener());
+		upperBody.setManufactuing(new PrepForManufacturing() {
+					public CSG prep(CSG arg0) {
+						return arg0.toZMin();
+					}
+				});
+		allCad.add(upperBody)
+		
+		
 		return allCad;
 	}
 };
