50b461e3144c9002346ef920c936cd563cbc6173
Octogonapus <firey45@gmail.com>
2016-04-12 10:47:19 -0400

    Update/n/n

--- linkOutput.groovy
+++ linkOutput.groovy
@@ -53,5 +53,6 @@ bar = bar.union(line);
 bar = bar.union(line.roty(90));
 bar = bar.union(line.roty(180));
 
-return bar.movey(inputLength / 2 + 9.5 - 9.925).movez(37 - 5.942);
+//return bar.movey(inputLength / 2 + 9.5 - 9.925).movez(37 - 5.942);
+return bar;
 }
