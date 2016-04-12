23c475a94a8c297e8f9ce1886684a2f0d4a4b583
Octogonapus <firey45@gmail.com>
2016-04-12 10:54:20 -0400

    Update/n/n

--- servoProtractor.groovy
+++ servoProtractor.groovy
@@ -117,5 +117,6 @@ CSG linkSphere3 = linkSphere2.difference(screwHole.movex(12.4).movez(-5))
 CSG linkSphere = linkSphere3.difference(screwHoles3)
  
 
-return linkSphere.rotz(90).movey(10).movez(45.5)
+//return linkSphere.rotz(90).movey(10).movez(45.5)
+return linkSphere.rotz(90)
 }
