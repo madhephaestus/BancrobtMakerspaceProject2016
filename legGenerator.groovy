1f6defbcd0f0080a68b74f9c4d58f91e6e2eff22
Octogonapus <firey45@gmail.com>
2016-04-27 09:59:55 -0400

    Added CSG cache

--- legGenerator.groovy
+++ legGenerator.groovy
@@ -2,6 +2,7 @@ import com.neuronrobotics.bowlerstudio.creature.ICadGenerator;
 import com.neuronrobotics.bowlerstudio.creature.CreatureLab;
 import org.apache.commons.io.IOUtils;
 import com.neuronrobotics.bowlerstudio.vitamins.*;
+import java.util.*
 //println "Loading STL file"
 // Load an STL file from a git repo
 // Loading a local file also works here
@@ -34,12 +35,21 @@ Closure generateProtractor = (Closure)ScriptingEngine
             
 // Load the .CSG from the disk and cache it in memory
 //CSG servo  = Vitamins.get(servoFile);
+
+//Cache parts
+CSG servoCache = generateServo(25);
+CSG protractorCache = generateProtractor();
+Map<Float, CSG> linkCache = new HashMap<Float, CSG>();
+
 return new ICadGenerator(){
 	@Override 
 	public ArrayList<CSG> generateCad(DHParameterKinematics d, int linkIndex) {
 		ArrayList<DHLink> dhLinks = d.getChain().getLinks()
 		ArrayList<CSG> allCad=new ArrayList<>()
 		int i=linkIndex;
+		
+//		println "Generating link: "+linkIndex
+		
 		DHLink dh = dhLinks.get(linkIndex)
 		// Hardware to engineering units configuration
 		LinkConfiguration conf = d.getLinkConfiguration(i);
@@ -48,7 +58,9 @@ return new ICadGenerator(){
 		// Transform used by the UI to render the location of the object
 		Affine manipulator = dh.getListener();
 //		CSG tmpSrv = servo.clone()
-		CSG tmpSrv = generateServo(25)
+
+//		println "Making servo"
+		CSG tmpSrv = servoCache
 					.movez(-dh.getD())
 					.rotz(-Math.toDegrees(dh.getTheta()))
 					.rotz(90 + Math.toDegrees(dh.getTheta()))
@@ -57,7 +69,13 @@ return new ICadGenerator(){
 		tmpSrv.setManipulator(manipulator)
 		allCad.add(tmpSrv)
 
-		CSG tmpLink = generateLink(dh.getR())
+		//println "Making bar"
+		if (!linkCache.keySet().contains(dh.getR()))
+		{
+			println "New bar"
+			linkCache.put(dh.getR(), generateLink(dh.getR()));
+		}
+		CSG tmpLink = linkCache.get(dh.getR())
 					.movez(-dh.getD())
 					.rotz(-Math.toDegrees(dh.getTheta()))
 					.rotz(90 + Math.toDegrees(dh.getTheta()))
@@ -66,7 +84,8 @@ return new ICadGenerator(){
 		tmpLink.setManipulator(manipulator)
 		allCad.add(tmpLink)
 
-		CSG tmpProtractor = generateProtractor()
+//		println "Making protractor"
+		CSG tmpProtractor = protractorCache
 					.movez(-dh.getD())
 					.rotz(-Math.toDegrees(dh.getTheta()))
 					.rotz(90 + Math.toDegrees(dh.getTheta()))
@@ -74,25 +93,25 @@ return new ICadGenerator(){
 					.rotx(Math.toDegrees(dh.getAlpha()))
 		tmpProtractor.setManipulator(manipulator)
 		allCad.add(tmpProtractor)
-		
-		println "Generating link: "+linkIndex
 
-		if(i==0){
-			// more at https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/DHLink.java
-			println dh
-			println "D = "+dh.getD()// this is the height of the link
-			println "R = "+dh.getR()// this is the radius of rotation of the link
-			println "Alpha = "+Math.toDegrees(dh.getAlpha())// this is the alpha rotation
-			println "Theta = "+Math.toDegrees(dh.getTheta())// this is the rotation about hte final like orentation
-			println conf // gets the link hardware map from https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/LinkConfiguration.java
-			println conf.getHardwareIndex() // gets the link hardware index
-			println conf.getScale() // gets the link hardware scale to degrees from link units
-			// more from https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/AbstractLink.java
-			println  "Max engineering units for link = " + abstractLink.getMaxEngineeringUnits() 
-			println  "Min engineering units for link = " + abstractLink.getMinEngineeringUnits() 
-			println "Position "+abstractLink.getCurrentEngineeringUnits()
-			println manipulator
-		}
+		println "Finished"
+
+//		if(i==0){
+//			// more at https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/DHLink.java
+//			println dh
+//			println "D = "+dh.getD()// this is the height of the link
+//			println "R = "+dh.getR()// this is the radius of rotation of the link
+//			println "Alpha = "+Math.toDegrees(dh.getAlpha())// this is the alpha rotation
+//			println "Theta = "+Math.toDegrees(dh.getTheta())// this is the rotation about hte final like orentation
+//			println conf // gets the link hardware map from https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/LinkConfiguration.java
+//			println conf.getHardwareIndex() // gets the link hardware index
+//			println conf.getScale() // gets the link hardware scale to degrees from link units
+//			// more from https://github.com/NeuronRobotics/java-bowler/blob/development/src/main/java/com/neuronrobotics/sdk/addons/kinematics/AbstractLink.java
+//			println  "Max engineering units for link = " + abstractLink.getMaxEngineeringUnits() 
+//			println  "Min engineering units for link = " + abstractLink.getMinEngineeringUnits() 
+//			println "Position "+abstractLink.getCurrentEngineeringUnits()
+//			println manipulator
+//		}
 		return allCad;
 	}
 	@Override 
