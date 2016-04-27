13edf35068211af88cb78e403925758f34692bbe
Octogonapus <firey45@gmail.com>
2016-04-27 11:12:06 -0400

    Changed parameter to screw head keepaway length

--- vexServo.groovy
+++ vexServo.groovy
@@ -5,7 +5,7 @@ import com.neuronrobotics.bowlerstudio.vitamins.*;
 return { double keepawayLength ->
 [servoBodyX = 19.7, servoBodyY = 39.7, servoBodyZ = 38.3]
 [postDiameter = 8, postTaper = 0.6, postSpaceBetween = 4.2, postHeight = 14.3]
-screwDiameter = 4.5;
+[screwDiameter = 4.5, screwLengthMax = 12.7, screwHeadDiameter = 6.62]
 [postBracketX = 5, postBracketY = 1, postBracketZ = 11.4, postBracketTaper = 3.4]
 [axleSlotX = 3.175, axleSlotY = 3.175, axleSlotZ = 5.2, axleSlotInsetY = 8]
 [shoulderDiameter = 11.6, shoulderTaper = 1.9, shoulderHeight = 1, shoulderInsetY = 3.1]
@@ -24,9 +24,15 @@ servo = servo.union(post)
 servo = servo.union(post.movey(postDiameter + postTaper + postSpaceBetween))
 
 //Screws
-CSG screw = new Cylinder(screwDiameter / 2, screwDiameter / 2, keepawayLength, 8).toCSG()
+CSG screw = new Cylinder(screwDiameter / 2, screwDiameter / 2, screwLengthMax, 8).toCSG()
 			.movey(-servoBodyY / 2 + postDiameter / 2 + postTaper)
-			.movez(servoBodyZ / 2);
+			.movez(servoBodyZ / 2 + screwLengthMax);
+
+CSG screwKeepaway = new Cylinder(screwHeadDiameter / 2, screwHeadDiameter / 2, keepawayLength, 8).toCSG()
+			.movey(-servoBodyY / 2 + postDiameter / 2 + postTaper)
+			.movez(servoBodyZ / 2 + screwLengthMax * 2);
+			
+screw = screw.union(screwKeepaway)
 
 servo = servo.union(screw)
 			.union(screw.movey(postDiameter + postTaper + postDiameter / 2));
@@ -53,7 +59,7 @@ CSG clutch = new Cylinder(clutchDiameter / 2, clutchDiameter / 2, clutchHeight, 
 servo = servo.union(clutch);
 
 //Axle
-CSG axle = new Cylinder((axleSlotX * 1.414) / 2, (axleSlotX * 1.414) / 2, keepawayLength, 8).toCSG()
+CSG axle = new Cylinder((axleSlotX * 1.414) / 2, (axleSlotX * 1.414) / 2, 20, 8).toCSG()
 			.movey(servoBodyY / 2 - shoulderDiameter / 2 - shoulderTaper / 2 - shoulderInsetY)
 			.movez(servoBodyZ / 2);
 
